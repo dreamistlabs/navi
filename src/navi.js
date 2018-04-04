291ca120a152c5ec12eb87b2a1765340cd4c9059
Johnny Hu <johnny@comradeagency.com>
2018-04-03 17:47:58 -0700

    Write more test

--- src/navi.js
+++ src/navi.js
@@ -3,7 +3,6 @@
  * License: MIT
  **/
 
-
 class Navi {
   constructor(opts = {}) {
     this.jqueryCheck(),
@@ -35,17 +34,29 @@ class Navi {
   }
 
   /*
-   * Check whether or not options is an object.
+   * Validate the data type of various properties passed into the options argument.
    */
   optionsCheck(options) {
     if (!(options instanceof Object)) {
       throw new TypeError('Type Error! The options argument must be an object');
     }
-    if (options['customAnimation'] && !(options['customAnimation'] instanceof Object)) {
-      throw new TypeError('Type Error! The customAnimation property must be an object');
-    }
     if (options['animation'] && !(options['animation'] instanceof String)) {
       throw new TypeError('Type Error! The animation property must be an string');
+    }
+    if (options['customAnimation']) {
+      const keys = Object.keys(options['customAnimation']);
+
+      if(!(options['customAnimation'] instanceof Object)) {
+        throw new TypeError('Type Error! The customAnimation property must be an object');
+      }
+      // console.log( Object.keys(options['customAnimation']) );
+      // for (let i = 0; i < keys.length; i++) {
+
+      // }
+      // if (Object.keys(options['customAnimation']).includes('start')) {
+      //   console.log('yay');
+      // }
+
     }
   }
 
@@ -62,59 +73,58 @@ class Navi {
   }
 
   executeScript() {
-
     // TODO: set active class on load. keep it dry!
     // TODO: allow users to user data- attributes?
-
     $( document ).scroll(() => {
       const scrollPosition = $( window ).scrollTop();
 
       this.tickCollection.each((i, el) => {
-          const sectionStart = this.sectionStartPositions[i],
-                sectionHeight = this.sectionHeights[i],
-                nextSectionStart = sectionStart + sectionHeight,
-                inCurrentSection = sectionStart <= scrollPosition && scrollPosition < nextSectionStart,
-                tickShouldBeActive = inCurrentSection && !$( el ).hasClass(this.activeTickClass);
+        const sectionStart = this.sectionStartPositions[i],
+              sectionHeight = this.sectionHeights[i],
+              nextSectionStart = sectionStart + sectionHeight,
+              inCurrentSection = sectionStart <= scrollPosition && scrollPosition < nextSectionStart,
+              tickShouldBeActive = inCurrentSection && !$( el ).hasClass(this.activeTickClass);
 
+        // console.log(
+        //   'idx:', idx,
+        //   'isCurrent:', inCurrentSection
+        // );
+
+        if (inCurrentSection) {
           // console.log(
-          //   'idx:', idx,
-          //   'isCurrent:', inCurrentSection
+          //   'currentStart:', sectionStart,
+          //   'scrollPos:', scrollPosition,
+          //   'nextStart:', nextSectionStart
           // );
+          const startEndValueRange = this.endingValue - this.startingValue;
+          const percentageOfDistanceScrolled = (scrollPosition - this.sectionStartPositions[i]) / sectionHeight;
 
-          if (inCurrentSection) {
-            // console.log(
-            //   'currentStart:', sectionStart,
-            //   'scrollPos:', scrollPosition,
-            //   'nextStart:', nextSectionStart
-            // );
-            const startEndValueRange = this.endingValue - this.startingValue;
-            const percentageOfDistanceScrolled = (scrollPosition - this.sectionStartPositions[i]) / sectionHeight;
+         // change border radius
+          const radius =  this.startingValue + startEndValueRange * percentageOfDistanceScrolled;
+          $( el ).css('border-radius', radius + '%');
+        }
 
-           // change border radius
-            const radius =  this.startingValue + startEndValueRange * percentageOfDistanceScrolled;
-            $( el ).css('border-radius', radius + '%');
-          }
+        // add and remove active tick class
+        if (tickShouldBeActive) {
+          $( el ).addClass(this.activeTickClass);
+        } else if (!tickShouldBeActive) {
+          $( el ).removeClass(this.activeTickClass);
+        }
 
-          // add and remove active tick class
-          if (tickShouldBeActive) {
-            $( el ).addClass(this.activeTickClass);
-          } else if (!tickShouldBeActive) {
-            $( el ).removeClass(this.activeTickClass);
-          }
-
-          // ensures that the starting and ending radius values are
-          // properly set when scrolling out of a section in cases
-          // where a user might scroll faster than the script can keep up.
-          if (!inCurrentSection && scrollPosition < sectionStart) {
-            $( el ).css('border-radius', this.startingValue+'%');
-          }
-          if (!inCurrentSection && scrollPosition > nextSectionStart) {
-            $( el ).css('border-radius', this.endingValue+'%');
-          }
+        // ensures that the starting and ending radius values are
+        // properly set when scrolling out of a section in cases
+        // where a user might scroll faster than the script can keep up.
+        if (!inCurrentSection && scrollPosition < sectionStart) {
+          $( el ).css('border-radius', this.startingValue+'%');
+        }
+        if (!inCurrentSection && scrollPosition > nextSectionStart) {
+          $( el ).css('border-radius', this.endingValue+'%');
+        }
 
       });
     });
   }
+
 }
 
 // module.exports = Navi;
--- test/navi.test.js
+++ test/navi.test.js
@@ -25,6 +25,8 @@ describe('Navi', function() {
   var defaultTickClass       = 'navi-item';
   var defaultSectionClass    = 'navi-section';
 
+  var acceptableKeys = ['start', 'finish'];
+
   describe('checks if jquery is loaded', function() {
     before(function() {
       window.jQuery = null;
@@ -41,38 +43,139 @@ describe('Navi', function() {
     });
   });
 
-  describe('a new Navi instance with no options argument', function() {
-    var navi;
+  describe('Properties', function() {
+    describe('with no options argument', function() {
+      var navi;
+
+      before(function() {
+        generateElements('indicators', defaultTickClass, 'navi-list');
+        generateElements('section', defaultSectionClass, 'main');
+        navi = new Navi();
+      });
+
+      it('has activeTickClass default to ' + defaultActiveTickClass, function() {
+        assert.strictEqual(navi.activeTickClass, defaultActiveTickClass);
+      });
+      it('has animationName default to null', function() {
+        assert.strictEqual(navi.animationName, null);
+      });
+      it('has customAnimation default to null', function() {
+        assert.strictEqual(navi.customAnimation, null);
+      });
+      it('has sectionClass default to ' + defaultSectionClass, function() {
+        assert.strictEqual(navi.sectionClass, defaultSectionClass);
+      });
+      it('has tickClass default to ' + defaultTickClass, function() {
+        assert.strictEqual(navi.tickClass, defaultTickClass);
+      });
+      it('the collection of navi ticks should not be empty', function() {
+        assert.notStrictEqual(navi.tickCollection.length, 0);
+      });
+      it('the collection of navi sections collection should not be empty', function() {
+        assert.notStrictEqual(navi.sectionCollection.length, 0);
+      });
+      it('the collection of navi section start positions should not be empty', function() {
+        assert.notStrictEqual(navi.sectionStartPositions.length, 0);
+      });
+      it('the collection of navi section heights should not be empty', function() {
+        assert.notStrictEqual(navi.sectionHeights.length, 0);
+      });
+
+      after(function() {
+        removeElements(defaultTickClass);
+        removeElements(defaultSectionClass);
+      });
+    });
+
+    describe('with options argument containing properties that exist in the DOM', function() {
+      var customActiveTickClass = 'custom-tick--current';
+      var customAnimationName   = 'custom-animation';
+      var customTickClass       = 'custom-item';
+      var customSectionClass    = 'custom-section';
+
+      before(function() {
+        generateElements('section', customSectionClass, 'main');
+        generateElements('indicators', customTickClass, 'navi-list');
+        navi = new Navi({
+          activeTickClass: customActiveTickClass,
+          animationName: customAnimationName,
+          customAnimation: {
+            start: {},
+            finish: {}
+          },
+          ticks: customTickClass,
+          sections: customSectionClass
+        });
+      });
+
+      it('the activeTickClass property should equal the given custom class: ' + customActiveTickClass, function() {
+        assert.strictEqual(navi.activeTickClass, customActiveTickClass);
+      });
+      it('the animationName property should equal the given custom class: ' + customAnimationName, function() {
+        assert.strictEqual(navi.animationName, customAnimationName);
+      });
+      it('the tickClass property should equal the given custom class: ' + customTickClass, function() {
+        assert.strictEqual(navi.tickClass, customTickClass);
+      });
+      it('the tickCollection property should not be empty', function() {
+        assert.notStrictEqual(navi.tickCollection.length, 0);
+      });
+      it('the sectionClass property should equal the given custom class: ' + customSectionClass, function() {
+        assert.strictEqual(navi.sectionClass, customSectionClass);
+      });
+      it('the sectionCollection should not be empty', function() {
+        assert.notStrictEqual(navi.sectionCollection.length, 0);
+      });
+      it('the sectionStartPositions property should not be empty', function() {
+        assert.notStrictEqual(navi.sectionStartPositions.length, 0);
+      });
+      it('the sectionHeights should not be empty', function() {
+        assert.notStrictEqual(navi.sectionHeights.length, 0);
+      });
+      describe('and a valid customAnimation property', function() {
+        it('should include a "' + acceptableKeys[0] + '" property/key', function() {
+          expect(Object.keys(navi['customAnimation'])).to.include(acceptableKeys[0]);
+        });
+        it('should include a "' + acceptableKeys[1] + '" property/key', function() {
+          expect(Object.keys(navi['customAnimation'])).to.include(acceptableKeys[1]);
+        });
+      });
+
+      after(function() {
+        removeElements(customTickClass);
+        removeElements(customSectionClass);
+      });
+    });
+  });
+
+  describe('with options argument containing properties that DO NOT exist in the DOM', function() {
+    var customTickClass       = 'invalid-item';
+    var customSectionClass    = 'invalid-section';
 
     before(function() {
-      generateElements('indicators', defaultTickClass, 'navi-list');
       generateElements('section', defaultSectionClass, 'main');
-      navi = new Navi();
+      generateElements('indicators', defaultTickClass, 'navi-list');
     });
 
-    it('has activeTickClass default to ' + defaultActiveTickClass, function() {
-      assert.strictEqual(navi.activeTickClass, defaultActiveTickClass);
+    describe('given a custom tick class, ' + customTickClass + ', if the tickCollection property is empty', function() {
+      it('throws a Missing Collection error', function() {
+        var regex = new RegExp('\\bMissing Collection\\b.*\\b' + customTickClass + '\\b');
+        expect(function() {
+          navi = new Navi({
+            ticks: customTickClass
+          });
+        }).to.throw(Error, regex);
+      });
     });
-    it('has animationName default to null', function() {
-      assert.strictEqual(navi.animationName, null);
-    });
-    it('has customAnimation default to null', function() {
-      assert.strictEqual(navi.customAnimation, null);
-    });
-    it('has sectionClass default to ' + defaultSectionClass, function() {
-      assert.strictEqual(navi.sectionClass, defaultSectionClass);
-    });
-    it('the collection of navi ticks should not be empty', function() {
-      assert.notStrictEqual(navi.tickCollection.length, 0);
-    });
-    it('the collection of navi sections collection should not be empty', function() {
-      assert.notStrictEqual(navi.sectionCollection.length, 0);
-    });
-    it('the collection of navi section start positions should not be empty', function() {
-      assert.notStrictEqual(navi.sectionStartPositions.length, 0);
-    });
-    it('the collection of navi section heights should not be empty', function() {
-      assert.notStrictEqual(navi.sectionHeights.length, 0);
+    describe('given a custom section class, ' + customTickClass + ', if the sectionCollection property is empty', function() {
+      it('throws a Missing Collection error', function() {
+        var regex = new RegExp('\\bMissing Collection\\b.*\\b' + customSectionClass + '\\b');
+      expect(function() {
+        navi = new Navi({
+          sections: customSectionClass
+        });
+      }).to.throw(Error, regex);
+      });
     });
 
     after(function() {
@@ -81,137 +184,48 @@ describe('Navi', function() {
     });
   });
 
-  describe('with options argument', function() {
-    var customTickClass       = 'custom-item';
-    var customSectionClass    = 'custom-section';
+  describe('Behavior', function() {
+    describe('when browser window is scrolled to a navi section', function() {
+      var index = Math.floor(Math.random() * 7);
 
-    describe('and a custom tick and section class that exist in the DOM', function() {
-      before(function() {
-        generateElements('section', customSectionClass, 'main');
-        generateElements('indicators', customTickClass, 'navi-list');
-        navi = new Navi({
-          ticks: customTickClass,
-          sections: customSectionClass
-        });
-      });
-
-      it('tick class should equal custom class: ' + customTickClass, function() {
-        assert.strictEqual(navi.tickClass, customTickClass);
-      });
-      it('section class should equal custom class: ' + customSectionClass, function() {
-        assert.strictEqual(navi.sectionClass, customSectionClass);
-      });
-      it('tick collection should not be empty', function() {
-        assert.notStrictEqual(navi.tickCollection.length, 0);
-      });
-      it('section collection should not be empty', function() {
-        assert.notStrictEqual(navi.sectionCollection.length, 0);
-      });
-      it('collection of section start positions should not be empty', function() {
-        assert.notStrictEqual(navi.sectionStartPositions.length, 0);
-      });
-      it('collection of section heights should not be empty', function() {
-        assert.notStrictEqual(navi.sectionHeights.length, 0);
-      });
-
-      after(function() {
-        removeElements(customTickClass);
-        removeElements(customSectionClass);
-      });
-    });
-
-    describe('and a custom tick and section class that DOES NOT exist in the DOM', function() {
-      before(function() {
+      before(async function() {
         generateElements('section', defaultSectionClass, 'main');
         generateElements('indicators', defaultTickClass, 'navi-list');
+        navi = await new Navi();
+        var sectionStartPosition = await navi.sectionStartPositions[index];
+        await window.scrollTo(0, sectionStartPosition);
       });
 
-      it('throws an error if unable to collect elements with custom ticks class', function() {
-        var regex = new RegExp('\\bMissing Collection\\b.*\\b' + customTickClass + '\\b');
-        expect(function() {
-          navi = new Navi({
-            ticks: customTickClass
-          });
-        }).to.throw(Error, regex);
-      });
-      it('throws an error if unable to collect elements with custom sections class', function() {
-        var regex = new RegExp('\\bMissing Collection\\b.*\\b' + customSectionClass + '\\b');
-        expect(function() {
-          navi = new Navi({
-            sections: customSectionClass
-          });
-        }).to.throw(Error, regex);
+      it('the corresponding nav indicator should have the active tick class', async function() {
+        const ticks = await document.getElementsByClassName(defaultTickClass);
+        assert.isTrue(ticks[index].classList.contains(defaultActiveTickClass));
       });
 
-      after(function() {
-        removeElements(defaultTickClass);
-        removeElements(defaultSectionClass);
-      });
-    });
-
-    describe('and a custom active tick class', function() {
-      var navi;
-      var customActiveTickClass = 'navi--active';
-
-      before(function() {
-        generateElements('section', defaultSectionClass, 'main');
-        generateElements('indicators', defaultTickClass, 'navi-list');
-        navi = new Navi({
-          activeTickClass: customActiveTickClass
+      it('only one nav indicator should have the active tick class', async function() {
+        const ticks = await document.getElementsByClassName(defaultTickClass);
+        const results = await Array.from(ticks).filter(function(tick) {
+          return tick.classList.contains(defaultActiveTickClass);
         });
+        assert.strictEqual(results.length, 1, 'Only one indicator should have the active class');
       });
-
-      it('active tick class should custom class, ' + customActiveTickClass, function() {
-        assert.strictEqual(navi.activeTickClass, customActiveTickClass);
-      });
-
-      after(function() {
-        removeElements(defaultTickClass);
-        removeElements(defaultSectionClass);
-      });
-    });
-  });
-
-  describe('when browser window is scrolled to a navi section', function() {
-    var index = Math.floor(Math.random() * 7);
-
-    before(async function() {
-      generateElements('section', defaultSectionClass, 'main');
-      generateElements('indicators', defaultTickClass, 'navi-list');
-      navi = await new Navi();
-      var sectionStartPosition = await navi.sectionStartPositions[index];
-      await window.scrollTo(0, sectionStartPosition);
-    });
-
-    it('the corresponding nav indicator should have the active tick class', async function() {
-      const ticks = await document.getElementsByClassName(defaultTickClass);
-      assert.isTrue(ticks[index].classList.contains(defaultActiveTickClass));
-    });
-
-    it('only one nav indicator should have the active tick class', async function() {
-      const ticks = await document.getElementsByClassName(defaultTickClass);
-      const results = await Array.from(ticks).filter(function(tick) {
-        return tick.classList.contains(defaultActiveTickClass);
-      });
-      assert.strictEqual(results.length, 1, 'Only one indicator should have the active class');
     });
   });
 
   describe('Error Handling', function() {
-    describe('an options argument that isn\'t an object', function() {
-      it('throws an Argument Error', function() {
-        expect(function() {
-          new Navi(50);
-        }).to.throw(TypeError, 'must be an object');
-      });
+    it('throws an error if options argument isn\'t an object', function() {
+      expect(function() {
+        new Navi(50)
+      }).to.throw(TypeError, 'must be an object');
     });
-
-    describe('a customAnimation value that isn\'t an object', function() {
-      it('throws an Argument Error', function() {
-        expect(function() {
-          new Navi({ customAnimation: 'notAnObject' });
-        }).to.throw(TypeError, 'must be an object');
-      });
+    it('throws an error if the customAnimation property\'s value isn\'t an object', function() {
+      expect(function() {
+        new Navi({ customAnimation: 'notAnObject' });
+      }).to.throw(TypeError, 'must be an object');
+    });
+    it('throws an error if the customAnimation property\'s keys don\'t include: ' + acceptableKeys[0] + ', ' + acceptableKeys[1], function() {
+      expect(function() {
+        new Navi({ customAnimation: { notStart: {}, notFinish: {}} });
+      }).to.throw(TypeError, 'must be an object');
     });
   });
 });
